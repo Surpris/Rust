67dc5c995eb943f75d7c18683d3f8164f33b2d7b
Toshiyuki Nishiyama Hiraki <take90-it09-easy27@outlook.jp>
2020-05-05 17:40:48 +0900

    add functions to test transpose (reversed_axes) and permute (self-made)

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Rust examples
 Repository for studying Rust and developing projects in Rust
+
+# crates to study
+* [ndarray](#ndarray)
+
+## ndarray 0.13.1
+For NumPy users, [the instruction provided by the ndarray crate](https://docs.rs/ndarray/0.13.1/ndarray/doc/ndarray_for_numpy_users/index.html) is very useful.
+
+### generate
+* `pub fn arr1<A: Clone>()xs: &[A] -> Array1<A>`
+    * `sum(&self) -> A where A: Clone + Add<Output = A> + num_traits::Zero`
+    * `into_shape<E>(self, shape: E) -> Result<ArrayBase<S, E::Dim>, ShapeError> where E: IntoDimension`
+* `pub fn arr2<A: Clone, V: FixedInitializer<Elem = A>>(xs: &[V]) -> Array2<A>`
+    * `shape(&self) -> &[usize]`
+    * `reversed_axes(mut self) -> ArrayBase<S, D>`
+* `pub type Array<A, D> = ArrayBase<OwnedRepr<A>, D>`
+    * `from(_: T) -> Self`
+
+### operate
+* `s!`: slice macro
+* `pub struct Axis(pub usize)`
+* `pub struct Slice`
+    * `fn from(_: T) -> Self`
+* Array
+    * `pub fn slice_axis(&self, axis: Axis, indices: Slice) -> ArrayView<'_, A, D> where S: Data`
+
+### arithmetic
+* `fn add(self, rhs: Rhs) -> Self::Output`
+    * `use std::ops::Add;` is required.
+* `fn sub(self, rhs: Rhs) -> Self::Output`
+    * `use std::ops::Sub;` is required.
+* `fn mul(self, rhs: Rhs) -> Self::Output`
+    * `use std::ops::Mul;` is required.
+* Array
+    * `pub fn dot<Rhs>(&self, rhs: &Rhs) -> <Self as Dot<Rhs>>::Output where Self: Dot<Rhs>`
--- src/ndarray_test/mod.rs
+++ src/ndarray_test/mod.rs
@@ -2,9 +2,16 @@
 //!
 //! provide test functions using ndarray crate
 
-// #[macro_use(s)]
-// extern crate ndarray;
-use ndarray::{arr1, arr2, Array};
+use ndarray::{arr1, arr2, Array, Array2, Axis, Slice};
+
+// for Array::zeros
+use num_traits::identities as num_id;
+
+// for add, sub, mul ops. of Array
+// these ops take `self`.
+use std::ops::{Add, Mul, Sub};
+
+/*** http://asukiaaa.blogspot.com/2018/01/rustndarray.html ***/
 
 /// test to call arr1 and arr2
 pub fn test_ndarray_call() -> bool {
@@ -29,5 +36,83 @@ pub fn test_ndarray_slice() -> bool {
     let vec1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
     let mat1 = Array::from(vec1).into_shape((3, 3)).unwrap();
     assert_eq!(mat1.slice(s![0..2, 1..3]), arr2(&[[2, 3], [5, 6]]));
+    true
+}
+
+/// test to transpose a matrix
+pub fn test_ndarray_transpose() -> bool {
+    let vec1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mat1 = Array::from(vec1)
+        .into_shape((3, 3))
+        .unwrap()
+        .reversed_axes();
+    assert_eq!(mat1, arr2(&[[1, 4, 7], [2, 5, 8], [3, 6, 9]]));
+    true
+}
+
+/// test to execute add, sub, dot, mul, etc.
+///
+/// {add, sub, mul} take `self`, but dot does not take `self.`
+pub fn test_ndarray_ops() -> bool {
+    let vec1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let vec2 = vec![1, 1, 1, 2, 2, 2, 3, 3, 3];
+    let mat1 = Array::from(vec1).into_shape((3, 3)).unwrap();
+    let mat2 = Array::from(vec2).into_shape((3, 3)).unwrap();
+    // println!("add: {:?}", mat1.add(&mat2));
+    println!("add: {:?}", mat1 + &mat2);
+
+    let vec1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mat1 = Array::from(vec1).into_shape((3, 3)).unwrap();
+    // println!("sub: {:?}", mat1.sub(&mat2));
+    println!("sub: {:?}", mat1 - &mat2);
+
+    let vec1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mat1 = Array::from(vec1).into_shape((3, 3)).unwrap();
+    println!("dot: {:?}", mat1.dot(&mat2));
+
+    // let vec1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    // let mat1 = Array::from(vec1).into_shape((3, 3)).unwrap();
+    // println!("mul: {:?}", mat1.mul(&mat2));
+    println!("mul: {:?}", mat1 * &mat2);
+    true
+}
+
+/// test to permute a 2D array
+///
+/// permute is not implemented in ndarray crate in default.
+pub fn test_ndarray_permute() -> bool {
+    let vec1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let vec2 = vec![2, 3, 1, 5, 6, 4, 8, 9, 7];
+    let mat1 = Array::from(vec1).into_shape((3, 3)).unwrap();
+    let mat2 = Array::from(vec2).into_shape((3, 3)).unwrap();
+    let mat3 = permute_2d(mat1, vec![1, 2, 0], 1);
+    assert_eq!(mat3, mat2);
+    true
+}
+
+/// permute 2D array
+fn permute_2d<T>(ary: Array2<T>, order: Vec<usize>, dim: usize) -> Array2<T>
+where
+    T: Clone + num_id::Zero,
+{
+    assert!(dim == (0 | 1));
+    assert_eq!(
+        ary.index_axis(Axis(dim), 0).len(),
+        order.len()
+    );
+    let mut buff: Array2<T> = Array::zeros(ary.raw_dim());
+    for ii in 0..order.len() {
+        buff.row_mut(ii).assign(&ary.index_axis(Axis(dim), order[ii]));
+    }
+    match dim {
+        0 => buff,
+        _ => buff.reversed_axes(),
+    }
+}
+
+/*** https://qiita.com/osanshouo/items/7342d92a28d2df0b259a ***/
+
+/// test to zeros, ones, from_elem,
+pub fn test_ndarray_functions() -> bool {
     true
 }
